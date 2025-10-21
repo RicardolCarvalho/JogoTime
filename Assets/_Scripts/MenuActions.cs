0c25294534856e9c5560aa5fcf4b03ac91209a50
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2025-10-21 14:56:09 -0300

    Novas Scenes criadas

--- Assets/_Scripts/MenuActions.cs
+++ Assets/_Scripts/MenuActions.cs
@@ -5,14 +5,11 @@ public class MenuActions : MonoBehaviour
 {
     public void IniciarJogo()
     {
-        GameController.ResumeGame(); // garante timeScale = 1
-        GameController.Init();
         SceneManager.LoadScene(1);
     }
 
     public void Menu()
     {
-        GameController.ResumeGame(); // garante timeScale = 1
         SceneManager.LoadScene(0);
     }
 }
