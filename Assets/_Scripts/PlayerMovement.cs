b10ec39b709ad88c14f18d7b58a9dfa3f1c393a5
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2025-10-27 14:35:00 -0300

    movimentação e animação do jogador prontos

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -1,25 +1,47 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 4f;
+    public Animator animator;
 
-    private Rigidbody2D rb;
-    public float speed;
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    Vector2 input;
+    Vector2 lastMoveDir = Vector2.down;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        if (!animator) animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    void Update() {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        if (input.sqrMagnitude > 1f) input.Normalize();
+
+        animator.SetFloat("MoveX", input.x);
+        animator.SetFloat("MoveY", input.y);
+        animator.SetFloat("Speed", input.sqrMagnitude);
+
+        if (input.sqrMagnitude > 0.0001f) lastMoveDir = input;
+        animator.SetFloat("LastX", lastMoveDir.x);
+        animator.SetFloat("LastY", lastMoveDir.y);
+
+        // flip só quando de fato é movimento lateral
+        if (Mathf.Abs(input.x) > Mathf.Abs(input.y) && Mathf.Abs(input.x) > 0.01f) {
+            sr.flipX = input.x > 0f;        // sprite base olha para ESQUERDA → direita = flipX true
+        } else {
+            sr.flipX = lastMoveDir.x > 0f;  // parado: mantém o último lado
+        }
+    }
+
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-
-        rb.MovePosition(rb.position + movement.normalized * speed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + input * speed * Time.fixedDeltaTime);
     }
 }
